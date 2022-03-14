9365ece99d22b990765649a93f77b8eb2c21ba13
Dogwasp <Dogwasp@users.noreply.github.com>
2022-03-14 15:37:02 +0000

    Add files via upload

---
+++ assets/tiled/units.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="units" tilewidth="32" tileheight="32" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="32" height="32" source="../assets/images/bow0.png"/>
+ </tile>
+ <tile id="1">
+  <image width="32" height="32" source="../assets/images/bow1.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="../assets/images/sword0.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="../assets/images/sword1.png"/>
+ </tile>
+</tileset>
