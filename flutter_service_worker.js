8b281c1ba8abcfc7c2df26eed67a3a145f341cdc
Dogwasp <Dogwasp@users.noreply.github.com>
2022-04-18 15:13:43 +0100

    Add files via upload

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -57,9 +57,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "f4c30e44627579e2b7419f65fe7e23e2",
-"/": "f4c30e44627579e2b7419f65fe7e23e2",
-"main.dart.js": "f387dd5e892f30976d3a8971ce308789",
+"index.html": "8ce4794610f6253ade27c97d43300c74",
+"/": "8ce4794610f6253ade27c97d43300c74",
+"main.dart.js": "1b702f27a1679f0bc9992b43e939f175",
 "manifest.json": "83d533e5ab22a8928a684b90a19bb48d",
 "version.json": "389a6e6e6f34312743ca5f36467c84b5"
 };
